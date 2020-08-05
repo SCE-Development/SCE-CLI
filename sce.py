9c427cf57482fd77722e93eb9e55b79d903bb9a9
Evan Ugarte <evanuxd@gmail.com>
2020-08-04 21:39:23 -0700

    made sce.py small

--- sce.py
+++ sce.py
@@ -1,31 +1,12 @@
 import argparse
-import sys
-import subprocess
-import os
-from os import path
 from tools.setup import SceSetupTool
 
-setup = SceSetupTool()
-# parser = argparse.ArgumentParser()
-
-# parser.add_argument('--setup', dest='setup', action='store_true')
-# args = parser.parse_args()
-
-# setup.check_os()
-#try:
-    #subprocess.check_call("sce", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-    #input('eyes')
-#except:
-#input('dark knight hindi subtitile 1080p free stream 60fps no virus')
-#os.environ["PATH"] += ';E:\\SJSU\\sce\\dev\\build\\exe.win-amd64-3.8'
-#print(os.environ["PATH"])
-
-# if args.setup:
-#     setup.setup()
-# else:
-#     print('did nothing wrong')
-
 parser = argparse.ArgumentParser()
-parser.add_argument('--test', dest='test', action='store_true')
+
+parser.add_argument(
+    'command', help='Setup for the SCE tool to run (setup, run, presubmit, etc.)')
 args = parser.parse_args()
-setup.add_sce_alias()
+
+if args.command == 'setup':
+    setup = SceSetupTool()
+    setup.setup()
