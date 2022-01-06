9f07f217136b1504ad4b593fcbe14054715812ef
Allen Tran <allentran@allens-macbook.local>
2022-01-06 15:59:42 -0800

    put back lint

--- sce.py
+++ sce.py
@@ -7,16 +7,19 @@ from tools.sce_presubmit_handler import ScePresubmitHandler
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "command", help="Setup for the SCE tool to run " + "(setup, run, presubmit, etc.)"
-)
+    'command', help='Setup for the SCE tool to run ' +
+    '(setup, run, presubmit, etc.)')
 parser.add_argument(
-    "--project", "-p", nargs="+", help="Project to run presubmit checks for."
-)
-parser.add_argument("--proto", nargs=1, help="The language(s) to generate proto code.")
+    '--project', '-p', nargs='+', help='Project to run presubmit checks for.')
 parser.add_argument(
-    "--language", nargs="+", help="The language(s) to generate proto code."
-)
-parser.add_argument("--service", "-s", nargs="*", help="SCE Service name")
+    '--proto', nargs=1,
+    help='The language(s) to generate proto code.')
+parser.add_argument(
+    '--language', nargs='+',
+    help='The language(s) to generate proto code.')
+parser.add_argument(
+    '--service', '-s', nargs='*',
+    help='SCE Service name')
 args = parser.parse_args()
 
 # cd into the dev folder if we are in windows.
@@ -26,18 +29,18 @@ if platform.system() == "Windows":
     place = os.environ["SCE_PATH"]
     os.chdir(place)
 
-if args.command == "setup":
+if args.command == 'setup':
     setup = SceSetupTool()
     setup.setup()
-elif args.command == "presubmit":
+elif args.command == 'presubmit':
     test_project = ScePresubmitHandler(args.project)
     test_project.handle_testing()
-elif args.command == "generate":
+elif args.command == 'generate':
     generator = SceProtoGenerator(args.proto[0], args.language)
     generator.handle_proto_generation()
-elif args.command == "run":
+elif args.command == 'run':
     handler = SceServiceHandler(args.service)
     handler.run_services()
-elif args.command == "test":
+elif args.command == 'test':
     setup = SceSetupTool()
     setup.add_sce_alias()
